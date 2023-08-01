91f484e9cc85e7fc17fa881bc6d4f40ca77e3906
DrowKid <135056994+drowkid-1@users.noreply.github.com>
2023-07-31 18:21:00 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js App Router',
+    default: 'DrowKid - Router',
     template: '%s | Next.js App Router',
   },
   description:
