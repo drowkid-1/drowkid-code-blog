6051d6c521d729b5d2f5091b7b40bd44f6429a8e
DrowKid <135056994+drowkid-1@users.noreply.github.com>
2023-07-31 18:46:11 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     title: 'Next.js App Router Playground',
     description:
       'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
-    images: [`/api/og?title=Next.js App Router`],
+    images: [`/api/og?title=Next.js DrowKid`],
   },
   twitter: {
     card: 'summary_large_image',
